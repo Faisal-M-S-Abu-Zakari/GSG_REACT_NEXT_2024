746ca2143b63d2229df0e59e2208ee06f44c5fbf
Faisal <zkryfysl@gmail.com>
2025-02-27 15:17:20 +0200

    use the link insted of making client component

--- nexthw/component/taskItem.tsx
+++ nexthw/component/taskItem.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import Image from "next/image";
 import complete from "../public/complete.jpg";
 import Pending from "../public/pending.jpg";
-import ButtonComponent from "./ButtonComponent";
+
+import Link from "next/link";
 
 interface IProps {
   task: todos.ITodo;
@@ -10,32 +11,38 @@ interface IProps {
 
 const TaskItem = ({ task }: IProps) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-xs mx-auto transition-transform hover:scale-105">
-      <h2 className="text-lg font-semibold text-gray-900 mb-3">
-        Title :{" "}
+    <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-xs mx-auto transition-transform hover:scale-105 transform hover:shadow-xl">
+      <h2 className="text-xl font-semibold text-gray-800 mb-3 truncate">
         {task.title.length > 20
           ? task.title.substring(0, 20) + "..."
           : task.title}
       </h2>
+
       <p
-        className={`py-1 mb-2 px-3 rounded-full text-sm font-medium w-fit ${
+        className={`py-1 mb-4 px-3 rounded-full text-sm font-medium w-fit ${
           task.completed
             ? "bg-green-100 text-green-600"
             : "bg-red-100 text-red-600"
         }`}
       >
-        Status :{task.completed ? "✅ Completed" : "🕛 Pending"}
+        Status: {task.completed ? "✅ Completed" : "🕛 Pending"}
       </p>
-      <div className="relative w-60 h-48">
+
+      <div className="relative w-full h-40 mb-4 rounded-md overflow-hidden">
         <Image
           src={task.completed ? complete : Pending}
           alt={task.completed ? "Completed Task" : "Pending Task"}
           layout="fill"
           objectFit="cover"
-          className="rounded-md"
         />
       </div>
-      <ButtonComponent id={task.id} />
+
+      <Link
+        href={`/taskDetails/${task.id}`}
+        className="mt-4 bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-all duration-300 transform hover:scale-105"
+      >
+        View Details
+      </Link>
     </div>
   );
 };

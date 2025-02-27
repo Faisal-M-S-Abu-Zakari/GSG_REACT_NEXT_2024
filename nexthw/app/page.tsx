82e1f47fd3eb45ad680b041b196f39fa95539193
Faisal <zkryfysl@gmail.com>
2025-02-27 14:41:38 +0200

    give the five task style using tailwind

--- nexthw/app/page.tsx
+++ nexthw/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-
+import React from "react";
 import TaskItem from "./../component/taskItem";
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 export default async function Home() {
@@ -7,11 +6,15 @@ export default async function Home() {
   const data = (await res.json()) as todos.ITodo[];
 
   return (
-    <div>
-      <h1>Task Tracker</h1>
-      {data.slice(0, 5).map((task) => (
-        <TaskItem task={task} key={task.id} />
-      ))}
+    <div className="min-h-screen bg-gray-100 py-10 px-5">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+        Task Tracker 📝
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
+        {data.slice(0, 5).map((task) => (
+          <TaskItem task={task} key={task.id} />
+        ))}
+      </div>
     </div>
   );
 }
--- nexthw/component/taskItem.tsx
+++ nexthw/component/taskItem.tsx
@@ -1,14 +1,38 @@
 import React from "react";
-
+import Image from "next/image";
+import complete from "../public/complete.jpg";
+import Pending from "../public/pending.jpg";
 interface IProps {
   task: todos.ITodo;
 }
 
 const TaskItem = ({ task }: IProps) => {
   return (
-    <div>
-      <h2>{task.title}</h2>
-      <p className="">{task.completed ? "Completed ✅" : "Pending ❌"}</p>
+    <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-xs mx-auto transition-transform hover:scale-105">
+      <h2 className="text-lg font-semibold text-gray-900 mb-3">
+        Title :{" "}
+        {task.title.length > 20
+          ? task.title.substring(0, 20) + "..."
+          : task.title}
+      </h2>
+      <p
+        className={`py-1 mb-2 px-3 rounded-full text-sm font-medium w-fit ${
+          task.completed
+            ? "bg-green-100 text-green-600"
+            : "bg-red-100 text-red-600"
+        }`}
+      >
+        Status :{task.completed ? "✅ Completed" : "🕛 Pending"}
+      </p>
+      <div className="relative w-60 h-50">
+        <Image
+          src={task.completed ? complete : Pending}
+          alt={task.completed ? "Completed Task" : "Pending Task"}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-md"
+        />
+      </div>
     </div>
   );
 };

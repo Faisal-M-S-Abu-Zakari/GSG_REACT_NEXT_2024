9b2cd1e34ce18b01b57c07c94950bd91769b1775
Faisal <zkryfysl@gmail.com>
2025-02-27 12:49:27 +0200

    create next app

--- HW2/src/context/contextTasks.jsx
+++ HW2/src/context/contextTasks.jsx
@@ -3,43 +3,46 @@ import { createContext, useState, useEffect } from "react";
 export const TasksContext = createContext();
 
 const TasksProvider = ({ children }) => {
-    const [tasks, setTasks] = useState(() => {
-        const savedTasks = localStorage.getItem("tasks");
-        return savedTasks ? JSON.parse(savedTasks) : [];
-    });
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
 
-    useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    }, [tasks]);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
-    const addTask = (task, isUrgent) => {
-        setTasks((prevTasks) => [
-            ...prevTasks,
-            { task, isUrgent, completed: false },
-        ]);
-    };
+  const addTask = (task, isUrgent) => {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { task, isUrgent, completed: false },
+    ]);
+  };
 
-    const toggleTaskCompletion = (index) => {
-        setTasks((prevTasks) =>
-            prevTasks.map((t, i) =>
-                i === index ? { ...t, completed: !t.completed } : t
+  const toggleTaskCompletion = (index) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((t, i) =>
+        i === index ? { ...t, completed: !t.completed } : t
+      )
+    );
+  };
 
-            )
-        );
-    };
+  const deleteTask = (index) => {
+    setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
+  };
 
-    const deleteTask = (index) => {
-        setTasks((prevTasks) => prevTasks.filter((_ , i) => i !== index));
-    };
+  const contextValues = {
+    tasks,
+    addTask,
+    toggleTaskCompletion,
+    deleteTask,
+  };
 
-    const contextValues = {
-        tasks,
-        addTask,
-        toggleTaskCompletion,
-        deleteTask,
-    };
-
-    return <TasksContext.Provider value={contextValues}>{children}</TasksContext.Provider>;
+  return (
+    <TasksContext.Provider value={contextValues}>
+      {children}
+    </TasksContext.Provider>
+  );
 };
 
 export default TasksProvider;

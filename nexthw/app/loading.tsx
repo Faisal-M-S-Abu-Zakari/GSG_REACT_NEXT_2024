cdcf6908bd98f4353ba6329a5da3d7f96ca6686e
Faisal <zkryfysl@gmail.com>
2025-03-02 15:04:11 +0200

    give loading page simple style

--- nexthw/app/loading.tsx
+++ nexthw/app/loading.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const loading = () => {
-  return <div>loading ...</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="flex items-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
+        <span className="ml-4 text-lg text-gray-700">Loading...</span>
+      </div>
+    </div>
+  );
 };
 
 export default loading;
